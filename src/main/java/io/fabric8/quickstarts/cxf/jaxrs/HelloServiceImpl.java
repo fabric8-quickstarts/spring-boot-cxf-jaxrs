92df6a7ba982829e668ef632876940edd594d6a6
Claus Ibsen <claus.ibsen@gmail.com>
2016-11-10 10:58:10 +0100

    https://github.com/fabric8io/ipaas-quickstarts/issues/1414 - Remove LoadBalancer as fmp does automatic expose it now

--- src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
+++ src/main/java/io/fabric8/quickstarts/cxf/jaxrs/HelloServiceImpl.java
@@ -15,21 +15,14 @@
  */
 package io.fabric8.quickstarts.cxf.jaxrs;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.springframework.stereotype.Service;
+import io.swagger.annotations.Api;
+import io.fabric8.quickstarts.cxf.jaxrs.HelloService;
  
-@Path("/sayHello")
-@Service
-public interface HelloService {
+@Api("/sayHello")
+public class HelloServiceImpl implements HelloService {
  
-    @GET
-    @Path("/{a}")
-    @Produces(MediaType.TEXT_PLAIN)
-    String sayHello(@PathParam("a") String a);
+    public String sayHello(String a) {
+        return "Hello " + a + ", Welcome to CXF RS Spring Boot World!!!";
+    }
     
 }
